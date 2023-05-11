fd6a2b8eb755ea97c9f2359cba291e03d39ab2db
Tiago <36941172+ti4gods@users.noreply.github.com>
2023-05-11 18:57:42 -0300

    Add files via upload

---
+++ src/modules/users/controllers/UsersController.ts
@@ -0,0 +1,28 @@
+import { Request, Response, response } from "express";
+import ListUsertService from "../services/ListUserService";
+import CreateUserService from "../services/CreateUserService";
+
+export default class UsersController {
+  public async index(request: Request, response: Response): Promise<Response> {
+
+    const listUser = new ListUsertService();
+
+    const users = await listUser.execute();
+
+    return response.json(users);
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password
+    });
+
+    return response.json(user);
+  }
+}
