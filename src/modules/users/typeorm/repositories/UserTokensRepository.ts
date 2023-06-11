6728070067bbcfdbae609d0950a08226dbd6791c
Tiago <36941172+ti4gods@users.noreply.github.com>
2023-06-11 14:46:37 -0300

    Add files via upload

---
+++ src/modules/users/typeorm/repositories/UserTokensRepository.ts
@@ -0,0 +1,31 @@
+import { EntityRepository, Repository } from "typeorm";
+import UserToken from "../entities/UserToken";
+
+@EntityRepository(UserToken)
+
+//Pesquisar o token
+class UserTokensRepository extends Repository<UserToken> {
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = await this.findOne({
+      where: {
+        token,
+      },
+    });
+
+    return userToken;
+  }
+
+  //Criar o token
+  public async generate(user_id: string): Promise<UserToken | undefined> {
+    const userToken = await this.create({
+      user_id,
+    });
+
+    await this.save(userToken);
+
+    return userToken;
+  }
+
+}
+
+export default UserTokensRepository
