f4189e4b94e5a805e004ac4b4e416fe2f2776fcf
Tiago <36941172+ti4gods@users.noreply.github.com>
2023-06-30 23:09:26 -0300

    Add files via upload

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -1,9 +1,9 @@
-import { getCustomRepository } from "typeorm";
-import AppError from "@shared/errors/AppError";
-import Order from "../typeorm/entities/Order";
-import OrdersRepository from "../typeorm/entities/repositories/OrderRepository";
 import CustomersRepository from "@modules/customers/typeorm/repositories/CustomersRepository";
 import ProductRepository from "@modules/products/typeorm/repositories/ProductsRepository";
+import AppError from "@shared/errors/AppError";
+import { getCustomRepository } from "typeorm";
+import Order from "../typeorm/entities/Order";
+import OrdersRepository from "../typeorm/repositories/OrdersRepository";
 
 interface IProduct {
   id: string;
@@ -16,9 +16,7 @@ interface IRequest {
 }
 
 class CreateOrderService {
-
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-
     const ordersRepository = getCustomRepository(OrdersRepository);
     const customersRepository = getCustomRepository(CustomersRepository);
     const productsRepository = getCustomRepository(ProductRepository);
@@ -26,28 +24,63 @@ class CreateOrderService {
     const customerExists = await customersRepository.findById(customer_id);
 
     if (!customerExists) {
-      throw new AppError('Could not find any customer with the given id.')
-    };
+      throw new AppError('Could not find any customer with the given id.');
+    }
 
     const existsProducts = await productsRepository.findAllByIds(products);
 
     if (!existsProducts.length) {
-      throw new AppError('Could not find any products with the given ids.')
+      throw new AppError('Could not find any products with the given ids.');
     }
 
-    const existsProductsIds = existsProducts.map((product) => product.id);
+    const existsProductsIds = existsProducts.map(product => product.id);
 
-    const chackInexistentProducts = products.filter(
-      product => !existsProductsIds.includes(product.id)
+    const checkInexistentProducts = products.filter(
+      product => !existsProductsIds.includes(product.id),
     );
 
-    if (chackInexistentProducts.length) {
+    if (checkInexistentProducts.length) {
       throw new AppError(
-        `Could not find product ${chackInexistentProducts[0].id}`
+        `Could not find product ${checkInexistentProducts[0].id}.`,
       );
     }
 
+    const quantityAvailable = products.filter(
+      product =>
+        existsProducts.filter(p => p.id === product.id)[0].quantity <
+        product.quantity,
+    );
 
+    if (quantityAvailable.length) {
+      throw new AppError(
+        `The quantity ${quantityAvailable[0].quantity}
+         is not available for ${quantityAvailable[0].id}.`,
+      );
+    }
+
+    const serializedProducts = products.map(product => ({
+      product_id: product.id,
+      quantity: product.quantity,
+      price: existsProducts.filter(p => p.id === product.id)[0].price,
+    }));
+
+    const order = await ordersRepository.createOrder({
+      customer: customerExists,
+      products: serializedProducts,
+    });
+
+    const { order_products } = order;
+
+    const updatedProductQuantity = order_products.map(product => ({
+      id: product.product_id,
+      quantity:
+        existsProducts.filter(p => p.id === product.product_id)[0].quantity -
+        product.quantity,
+    }));
+
+    await productsRepository.save(updatedProductQuantity);
+
+    return order;
   }
 }
 
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,4 +1,5 @@
 import customersRouter from '@modules/customers/routes/customers.routes';
+import ordersRouter from '@modules/orders/routes/orders.routes';
 import productsRouter from '@modules/products/routes/products.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
 import profileRouter from '@modules/users/routes/profile.routes';
@@ -13,6 +14,7 @@ routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
 routes.use('/profile', profileRouter);
-routes.use('/customers', customersRouter)
+routes.use('/customers', customersRouter);
+routes.use('/orders', ordersRouter);
 
 export default routes;
