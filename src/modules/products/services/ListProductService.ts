5bd1cb7fe97826f4e20cc0bdfc25ff5d718444db
Tiago <36941172+ti4gods@users.noreply.github.com>
2023-07-08 15:32:15 -0300

    Redis

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -1,14 +1,19 @@
 import { getCustomRepository } from "typeorm";
 import { ProductRepository } from "../typeorm/repositories/ProductsRepository";
-import AppError from "@shared/errors/AppError";
 import Product from "../typeorm/entities/Product";
+import  RedisCache from '@shared/cache/RedisCache';
 
 class ListProductService {
 
   public async execute(): Promise<Product[]> {
 
     const productsRepository = getCustomRepository(ProductRepository);
+
+    const redisCache = new RedisCache();
+
     const products = productsRepository.find();
+
+    await redisCache.save('teste', 'teste');
 
     return products;
   }
